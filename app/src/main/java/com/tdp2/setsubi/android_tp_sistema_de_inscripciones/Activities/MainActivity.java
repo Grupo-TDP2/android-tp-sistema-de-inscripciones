ce9398d03aa475a42cdbda3613bda64a5851eb32
gmerino92 <merino.gonza@gmail.com>
2018-09-15 16:16:38 -0300

    Repository initialization. First commit.

---
+++ app/src/main/java/com/tdp2/setsubi/android_tp_sistema_de_inscripciones/Activities/MainActivity.java
@@ -0,0 +1,15 @@
+package com.tdp2.setsubi.android_tp_sistema_de_inscripciones.Activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.tdp2.setsubi.android_tp_sistema_de_inscripciones.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
