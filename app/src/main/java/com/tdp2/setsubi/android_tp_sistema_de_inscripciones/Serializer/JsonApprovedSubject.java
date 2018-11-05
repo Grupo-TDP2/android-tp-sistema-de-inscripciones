4f95b3d8d5651668881298f16ae66fe6aeaa5756
tobiasbianchi <tobias.bianchi93@gmail.com>
2018-11-04 23:03:42 -0300

    implemnted and tested approved subject serializer and serivces

--- app/src/main/java/com/tdp2/setsubi/android_tp_sistema_de_inscripciones/Serializer/JsonApprovedSubject.java
+++ app/src/main/java/com/tdp2/setsubi/android_tp_sistema_de_inscripciones/Serializer/JsonApprovedSubject.java
@@ -2,54 +2,69 @@ package com.tdp2.setsubi.android_tp_sistema_de_inscripciones.Serializer;
 
 import com.google.gson.JsonElement;
 import com.tdp2.setsubi.android_tp_sistema_de_inscripciones.Models.ApprovedSubject;
+import com.tdp2.setsubi.android_tp_sistema_de_inscripciones.Models.Department;
+import com.tdp2.setsubi.android_tp_sistema_de_inscripciones.Models.Subject;
+import com.tdp2.setsubi.android_tp_sistema_de_inscripciones.Utils.AnyNullCheck;
+import com.tdp2.setsubi.android_tp_sistema_de_inscripciones.Utils.JsonGetter;
 
-public class JsonApprovedSubject extends JsonTransformer<ApprovedSubject> {
+import java.text.ParseException;
+import java.util.Date;
 
-    /*"{
-              \"subject\": {"+
-    "            \"id\": 90,"+
-    "            \"name\": \"Análisis matemático II A\","+
-    "            \"code\": \"03\","+
-    "            \"credits\": 6,"+
-    "            \"department_id\": 91,"+
-    "            \"created_at\": \"2018-11-04T16:57:05.689Z\","+
-    "            \"updated_at\": \"2018-11-04T16:57:05.689Z\""+
-    "        },"+
-    "        \"department\": {"+
-    "            \"id\": 91,"+
-    "            \"name\": \"Departamento de Matemática\","+
-    "            \"code\": \"61\","+
-    "            \"created_at\": \"2018-11-04T16:57:05.608Z\","+
-    "            \"updated_at\": \"2018-11-04T16:57:05.608Z\""+
-    "        },"+
-    "        \"enrolment\": {"+
-    "            \"id\": 101,"+
-    "            \"type\": \"normal\","+
-    "            \"student_id\": 70,"+
-    "            \"course_id\": 153,"+
-    "            \"created_at\": \"2018-11-04T16:57:07.935Z\","+
-    "            \"updated_at\": \"2018-11-04T16:57:07.935Z\","+
-    "            \"status\": \"not_evaluated\","+
-    "            \"final_qualification\": 6,"+
-    "            \"partial_qualification\": null"+
-    "        },"+
-    "        \"school_term\": {"+
-    "            \"id\": 73,"+
-    "            \"term\": \"first_semester\","+
-    "            \"year\": 2018,"+
-    "            \"date_start\": \"2018-03-12\","+
-    "            \"date_end\": \"2018-07-02\","+
-    "            \"created_at\": \"2018-11-04T16:57:05.810Z\","+
-    "            \"updated_at\": \"2018-11-04T16:57:05.810Z\""+
-    "        }" + 
-    "}"*/
+public class JsonApprovedSubject extends JsonTransformer<ApprovedSubject>
+{
     @Override
     public ApprovedSubject transform(JsonElement object)
     {
         if( object.isJsonObject() )
         {
-            JsonGetter getter = new JsonGetter();
+            JsonGetter getter = new JsonGetter(object.getAsJsonObject());
+            JsonGetter subjectJson = getter.atObject(JsonKeys.SUBJECT);
+            JsonGetter departmentJson = getter.atObject(JsonKeys.DEPARTMENT);
+            JsonGetter enrolmentJson = getter.atObject(JsonKeys.ENROLMENT);
+
+            Subject subject = getSubject(subjectJson,departmentJson);
+            Integer calification = enrolmentJson.asInt(JsonKeys.FINAL_QUALIFICATION);
+            String dateString = enrolmentJson.asString(JsonKeys.UPDATED_AT);
+            if(AnyNullCheck.any(subject, calification, dateString) ) return null;
+
+            Date date;
+            try {
+                date = JsonTransformHelper.getDate(dateString);
+            } catch (ParseException e) {
+                return null;
+            }
+
+            return new ApprovedSubject(subject,date,calification);
         }
         return null;
+    }
+
+    private Subject getSubject(JsonGetter subjectJson, JsonGetter departmentJson) {
+
+        Department department = getDepartment(departmentJson);
+        if( department == null ) return null;
+
+        Integer id = subjectJson.asInt(JsonKeys.ID),
+                code,
+                credits = subjectJson.asInt(JsonKeys.CREDITS);
+        String name = subjectJson.asString(JsonKeys.NAME);
+        String codeString = subjectJson.asString(JsonKeys.CODE);
+        if( codeString == null ) return null;
+        code = Integer.parseInt(codeString);
+
+        if( AnyNullCheck.any(id, code, credits, name, codeString) ) return null;
+
+        return new Subject(id, code, name, credits, department);
+    }
+
+    private Department getDepartment(JsonGetter departmentJson) {
+        Integer id = departmentJson.asInt(JsonKeys.ID), code;
+        String name = departmentJson.asString(JsonKeys.NAME);
+        String codeString = departmentJson.asString(JsonKeys.CODE);
+        if( codeString == null ) return null;
+        code = Integer.parseInt(codeString);
+
+        if( AnyNullCheck.any(id, code, name) ) return null;
+        return new Department(id, name, code);
     }
 }
